c18e4c4cc1d3d6fe001abe9b8e8ad2266f2177ab
之之 <99943354@qq.com>
2022-06-10 02:49:47 -0400

    added project

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const Express = require("express")();
+const Http = require("http").Server(Express);
+const Socketio = require("socket.io")(Http, {
+    cors: {
+        credentials: true
+    }
+});
+
+var position = {
+    x:200,
+    y:200
+};
+
+Socketio.on("connection", socket => {
+    socket.emit("position", position);
+});
+
+Http.listen(3000, () => {
+    console.log("Listening at :3000...")
+});
